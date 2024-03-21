f5b04b9fffab632ce7f5d7254977f37e7f4f4861
Maryna Bolotska <m.bolotska@icloud.com>
2024-03-21 10:54:41 +0100

    fix header image

--- NewsToday/NewsToday/Application/SceneDelegate.swift
+++ NewsToday/NewsToday/Application/SceneDelegate.swift
@@ -7,7 +7,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: OnboardingViewController())
+        let navigationController = UINavigationController(rootViewController: DetailsViewController())
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
--- NewsToday/NewsToday/Moduls/Details/View/DetailsViewController.swift
+++ NewsToday/NewsToday/Moduls/Details/View/DetailsViewController.swift
@@ -6,7 +6,7 @@ protocol DetailsViewControllerProtocol: AnyObject {
     func displayData()
 }
 
-final class DetailsViewController: UIViewController, DetailsViewControllerProtocol {
+final class DetailsViewController: UIViewController, DetailsViewControllerProtocol, UITextViewDelegate {
     
     //MARK: - Presenter
     private let presenter = DetailsPresenter()
@@ -17,6 +17,7 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
     weak private var detailsPresenterProtocol: DetailsPresenterProtocol?
     
     //MARK: - UI Components
+    private var headerHeightConstraint: NSLayoutConstraint!
     private let contentImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "image")
@@ -36,15 +37,24 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
         return view
     }()
     
-    private let categoryLabel = UILabel.makeLabel(font: UIFont.InterSemiBold(ofSize: 12), textColor: .white, numberOfLines: 1)
-    private let headlineLabel = UILabel.makeLabel(font: UIFont.InterBold(ofSize: 20), textColor: .white, numberOfLines: 2)
-    private let authorLabel = UILabel.makeLabel(font: UIFont.InterSemiBold(ofSize: 16), textColor: .white, numberOfLines: 1)
-    private let authorGreyLabel = UILabel.makeLabel(text: "Author", font: UIFont.InterRegular(ofSize: 14), textColor: UIColor.greyLight, numberOfLines: 1)
-    private let titleLabel = UILabel.makeLabel(font: UIFont.InterSemiBold(ofSize: 16), textColor: UIColor.blackPrimary, numberOfLines: 1)
+    private let categoryLabel = UILabel.makeLabel(font: UIFont.InterSemiBold(ofSize: 12), 
+                                                  textColor: .white,
+                                                  numberOfLines: 1)
+    private let headlineLabel = UILabel.makeLabel(font: UIFont.InterBold(ofSize: 20), 
+                                                  textColor: .white,
+                                                  numberOfLines: 2)
+    private let authorLabel = UILabel.makeLabel(font: UIFont.InterSemiBold(ofSize: 16), 
+                                                textColor: .white,
+                                                numberOfLines: 1)
+    private let authorGreyLabel = UILabel.makeLabel(text: "Author", 
+                                                    font: UIFont.InterRegular(ofSize: 14), textColor: UIColor.greyLight,
+                                                    numberOfLines: 1)
+    private let titleLabel = UILabel.makeLabel(font: UIFont.InterSemiBold(ofSize: 16), 
+                                               textColor: UIColor.blackPrimary,
+                                               numberOfLines: 1)
     private let contentTextView: UITextView = {
         let textView = UITextView()
         textView.isEditable = false
-        textView.isScrollEnabled = true
         textView.font = UIFont.InterRegular(ofSize: 16)
         textView.textColor = UIColor.greyDark
         return textView
@@ -52,18 +62,13 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
         setupConstraints()
-        view.backgroundColor = .systemBackground
-       
         presenter.setDetailsViewControllerProtocol(detailsViewControllerProtocol: self)
         self.detailsPresenterProtocol = presenter
         self.detailsPresenterProtocol?.getData()
-        
-        let bookmarkButton = UIBarButtonItem(image: UIImage(named: "bookmark-icon"), style: .plain, target: self, action: #selector(bookmarkButtonTapped))
-        bookmarkButton.tintColor = .white
-        navigationItem.rightBarButtonItem = bookmarkButton
+        configureController()
         setNavigationBar(title: "")
-      
     }
     // MARK: - Public Methods
     func setupData(with testData: ([DetailsModel])) {
@@ -89,21 +94,27 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
        }
     
     // MARK: Private methods
+    private func setupViews() {
+        [contentImage, shareButton, labelView, categoryLabel, headlineLabel, authorLabel, authorGreyLabel, titleLabel, contentTextView].forEach {view.addSubview($0) }
+        labelView.addSubview(categoryLabel)
+    }
+    
+    private func configureController() {
+        let bookmarkButton = UIBarButtonItem(image: UIImage(named: "bookmark-icon"), style: .plain, target: self, action: #selector(bookmarkButtonTapped))
+        bookmarkButton.tintColor = .white
+        navigationItem.rightBarButtonItem = bookmarkButton
+        headerHeightConstraint = contentImage.heightAnchor.constraint(equalToConstant: 70)
+        headerHeightConstraint.isActive = true
+        view.backgroundColor = .systemBackground
+        contentTextView.delegate = self
+    }
     
     private func setupConstraints() {
-        view.addSubview(contentImage)
-        view.addSubview(shareButton)
-        view.addSubview(labelView)
-        labelView.addSubview(categoryLabel)
-        view.addSubview(headlineLabel)
-        view.addSubview(authorLabel)
-        view.addSubview(authorGreyLabel)
-        view.addSubview(titleLabel)
-        view.addSubview(contentTextView)
-        
+        contentImage.clipsToBounds = true
         contentImage.snp.makeConstraints { make in
             make.top.equalToSuperview()
-            make.leading.trailing.equalToSuperview()
+           make.leading.trailing.equalToSuperview()
+   
         }
         shareButton.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(120)
@@ -138,16 +149,49 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
         }
         
         titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(392)
+            make.top.equalTo(contentImage.snp.bottom).offset(20)
             make.leading.equalToSuperview().offset(20)
         }
         contentTextView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(424)
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
             make.bottom.equalToSuperview()
         }
     }
     
- 
+    private func animateHeader() {
+        self.headerHeightConstraint.constant = 350
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
 }
+
+
+extension DetailsViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y < 0 {
+            self.headerHeightConstraint.constant += abs(scrollView.contentOffset.y)
+        } else if scrollView.contentOffset.y > 0 && self.headerHeightConstraint.constant >= 65 {
+            self.headerHeightConstraint.constant -= scrollView.contentOffset.y / 100
+            
+            if self.headerHeightConstraint.constant < 65 {
+                self.headerHeightConstraint.constant = 65
+            }
+        }
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if self.headerHeightConstraint.constant > 150 {
+            animateHeader()
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if self.headerHeightConstraint.constant > 150 {
+            animateHeader()
+        }
+    }
+}
+
