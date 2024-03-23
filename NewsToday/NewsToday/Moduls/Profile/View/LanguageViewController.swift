d474afc5e0e951b87275a2db214076778e8ee06f
Андрей Линьков <andrewlink15@gmail.com>
2024-03-23 15:36:09 +0300

    profile screen 3x

---
+++ NewsToday/NewsToday/Moduls/Profile/View/LanguageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LanguageViewController.swift
+//  NewsToday
+//
+//  Created by Андрей Линьков on 23.03.2024.
+//
+
+import Foundation
---
+++ NewsToday/NewsToday/Moduls/Profile/View/Terms&ConditionsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  Terms&ConditionsViewController.swift
+//  NewsToday
+//
+//  Created by Андрей Линьков on 22.03.2024.
+//
+
+import Foundation
