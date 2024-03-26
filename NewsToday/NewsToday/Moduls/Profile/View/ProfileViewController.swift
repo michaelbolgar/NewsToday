d98328cc4f1de19f2ec58dfd413771d807908fb7
Андрей Линьков <andrewlink15@gmail.com>
2024-03-26 17:07:45 +0300

    Profile screen final

--- NewsToday/NewsToday/Moduls/Profile/View/ProfileViewController.swift
+++ NewsToday/NewsToday/Moduls/Profile/View/ProfileViewController.swift
@@ -4,20 +4,25 @@ protocol ProfileViewControllerProtocol: AnyObject {
 
 }
 
-#warning("исправить следующие моменты:")
 /*
 
-1. присвоение цвета осуществляется через extension (см.папку Extensions), там есть цвет .greyLighter, который можно использовать по всему проекту. Таким образом, не надо будет постоянно писать цвет по rgb в коде + можно будет поменять этот цвет в два касания
+1.  DONE 
+ присвоение цвета осуществляется через extension (см.папку Extensions), там есть цвет .greyLighter, который можно использовать по всему проекту. Таким образом, не надо будет постоянно писать цвет по rgb в коде + можно будет поменять этот цвет в два касания
 
- 2. тексты кнопок должны быть реализованы через .makeLabel, не UITextView. Сейчас пользователь может стереть этот текст из кнопок
+ 2. DONE
+ тексты кнопок должны быть реализованы через .makeLabel, не UITextView. Сейчас пользователь может стереть этот текст из кнопок
 
- 3. картинки (стрелки, log out) лучше добавить в ассеты и вытягивать по имени. Это тоже обеспечит простой доступ и возможность легко изменить картинку по всему проекту
+ 3. DONE
+ картинки (стрелки, log out) лучше добавить в ассеты и вытягивать по имени. Это тоже обеспечит простой доступ и возможность легко изменить картинку по всему проекту
 
- 4. добавление элементов на экран и констрейнты лучше разнести по разным функциям, например setupView() и setupConstraints(). Это будет соответствовать принципу разделения ответственности по SOLID
+ 4. DONE
+ добавление элементов на экран и констрейнты лучше разнести по разным функциям, например setupView() и setupConstraints(). Это будет соответствовать принципу разделения ответственности по SOLID
 
- 5. большое количество элементов можно добавить на экран с помощью массива этих самых элементов и перебора, например: [label, label, view].forEach { view.addSubview($0) }
+ 5. DONE
+ большое количество элементов можно добавить на экран с помощью массива этих самых элементов и перебора, например: [label, label, view].forEach { view.addSubview($0) }
 
- 6. поправить вёрстку под SE (запусти на симуляторе через SE, увидишь куда уехало). Адаптивный лэйаут строится на том, что мы задаём длину/ширину элементов и их расположение, отталкиваясь от safeAreaLayoutGuide (в данном случае можно начать размещать нижнюю кнопку от view.bottom. Мы не задаём ширину width, а прибиваем левый и правый края вью относительно основной вью:
+ 6. DONE
+ поправить вёрстку под SE (запусти на симуляторе через SE, увидишь куда уехало). Адаптивный лэйаут строится на том, что мы задаём длину/ширину элементов и их расположение, отталкиваясь от safeAreaLayoutGuide (в данном случае можно начать размещать нижнюю кнопку от view.bottom. Мы не задаём ширину width, а прибиваем левый и правый края вью относительно основной вью:
 
  languageButton.snp.makeConstraints { make in
      make.leading.trailing.equalToSuperview().inset(20)
@@ -37,126 +42,83 @@ final class ProfileViewController: UIViewController {
     
     private let languageButton: UIView = {
         let element = UIView()
-        element.backgroundColor = UIColor(red: 243/255,
-                                          green: 244/255,
-                                          blue: 246/255,
-                                          alpha: 1)
+        element.backgroundColor = .greyLighter
         element.layer.cornerRadius = 12
         return element
     }()
     
-    private let language: UITextView = {
-        let element = UITextView()
-        element.text = "Language"
-        element.textColor = .darkGray
-        element.backgroundColor = UIColor(red: 243/255,
-                                          green: 244/255,
-                                          blue: 246/255,
-                                          alpha: 1)
-        element.font = .InterRegular(ofSize: 16)
-        return element
-    }()
+    private let language = UILabel.makeLabel(text: "Language",
+                                            font: UIFont.InterSemiBold(ofSize: 16),
+                                            textColor: UIColor.darkGray,
+                                            numberOfLines: 1)
     
     private let rightAngleL: UIImageView = {
         let element = UIImageView()
-        element.image = #imageLiteral(resourceName: "Right angle")
+        element.image = UIImage(named: "RightAngle")
         return element
     }()
+    
     
     let profileTitle = UILabel.makeLabel(text: "Profile",
                                   font: UIFont.InterBold(ofSize: 24),
                                   textColor: .blackPrimary,
                                   numberOfLines: nil)
     
-// переделать надпись профиль в UITextView
-//    private let profileTitle: UITextView = {
-//        let element = UITextView()
-//        element.text = "Profile"
-//        element.textColor = .blackPrimary
-//        element.font = .InterBold(ofSize: 24)
-//        return element
-//    }()
-    
     let pageTitle = UIView()
     
     private let profileImage: UIImageView = {
         let element = UIImageView()
-        element.image = #imageLiteral(resourceName: "Profile foto")
+        element.image = UIImage(named: "ProfileFoto")
         element.layer.cornerRadius = 35
         element.clipsToBounds = true
         return element
     }()
 
-    private let profileName: UITextView = {
-        let element = UITextView()
-        element.text = "Dev P"
-        element.textColor = .blackPrimary
-        element.font = .InterBold(ofSize: 16)
-        return element
-    }()
+    private let profileName = UILabel.makeLabel(text: "Dev P",
+                                                font: UIFont.InterSemiBold(ofSize: 16),
+                                                textColor: UIColor.blackDark,
+                                                numberOfLines: 1)
     
-    private let profileEmail: UITextView = {
-        let element = UITextView()
-        element.text = "dev@gmail.com"
-        element.textColor = .greyPrimary
-        element.font = .InterRegular(ofSize: 14)
-        return element
-    }()
-    
+    private let profileEmail = UILabel.makeLabel(text: "dev@gmail.com",
+                                                 font: UIFont.InterSemiBold(ofSize: 14),
+                                                 textColor: UIColor.greyLight,
+                                                 numberOfLines: 1)
+             
     private let termsButton: UIView = {
         let element = UIView()
-        element.backgroundColor = UIColor(red: 243/255,
-                                          green: 244/255,
-                                          blue: 246/255,
-                                          alpha: 1)
+        element.backgroundColor = .greyLighter
         element.layer.cornerRadius = 12
         return element
     }()
     
-    private let termsConditions: UITextView = {
-        let element = UITextView()
-        element.text = "Terms & Conditions"
-        element.textColor = .darkGray
-        element.backgroundColor = UIColor(red: 243/255,
-                                          green: 244/255,
-                                          blue: 246/255,
-                                          alpha: 1)
-        element.font = .InterRegular(ofSize: 16)
-        return element
-    }()
+    private let termsConditions  = UILabel.makeLabel(text: "Terms & Conditions",
+                                                     font: UIFont.InterSemiBold(ofSize: 16),
+                                                     textColor: UIColor.darkGray,
+                                                     numberOfLines: 1)
+    
     
     private let rightAngleT: UIImageView = {
         let element = UIImageView()
-        element.image = #imageLiteral(resourceName: "Right angle")
+        element.image = UIImage(named: "RightAngle")
         return element
     }()
     
     
     private let signoutButton: UIView = {
         let element = UIView()
-        element.backgroundColor = UIColor(red: 243/255,
-                                          green: 244/255,
-                                          blue: 246/255,
-                                          alpha: 1)
+        element.backgroundColor = .greyLighter
         element.layer.cornerRadius = 12
         return element
     }()
     
-    private let signOut: UITextView = {
-        let element = UITextView()
-        element.text = "Sign Out"
-        element.textColor = .darkGray
-        element.backgroundColor = UIColor(red: 243/255,
-                                          green: 244/255,
-                                          blue: 246/255,
-                                          alpha: 1)
-        element.font = .InterRegular(ofSize: 16)
-        return element
-    }()
+    private let signOut  = UILabel.makeLabel(text: "Sign Out",
+                                             font: UIFont.InterSemiBold(ofSize: 16),
+                                             textColor: UIColor.darkGray,
+                                             numberOfLines: 1)
     
     private let signOutLabel: UIImageView = {
         let element = UIImageView()
-        element.image = #imageLiteral(resourceName: "Sign Out")
+        element.image = UIImage(named: "SignOut")
         return element
     }()
     
@@ -166,40 +128,28 @@ final class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        layout()
-        //setupViews()
+        setupViews()
+        setupConstraints()
         
     }
     
     
     //MARK: Private Methods
-    private func layout() {
+    private func setupViews() {
         
-        view.addSubview(languageButton)
-        view.addSubview(profileTitle)
-        view.addSubview(pageTitle)
-        view.addSubview(profileImage)
-        view.addSubview(profileName)
-        view.addSubview(profileEmail)
-        view.addSubview(language)
-        view.addSubview(termsButton)
-        view.addSubview(termsConditions)
-        view.addSubview(signoutButton)
-        view.addSubview(signOut)
-        view.addSubview(rightAngleL)
-        view.addSubview(rightAngleT)
-        view.addSubview(signOutLabel)
-        
+        [languageButton, profileTitle, pageTitle, profileImage, profileName, profileEmail, language, termsButton, termsConditions, signoutButton, signOut, rightAngleL, rightAngleT, signOutLabel].forEach { view.addSubview($0)}
+    }
+    
+    private func setupConstraints() {
         
         languageButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(20)
             make.top.equalTo(view.safeAreaLayoutGuide).inset(208)
-            make.width.equalTo(336)
             make.height.equalTo(56)
         }
         
         profileTitle.snp.makeConstraints { make in
-            make.top.equalTo(view).inset(72)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(28)
             make.left.equalTo(view).inset(20)
         }
         
@@ -211,22 +161,22 @@ final class ProfileViewController: UIViewController {
         }
         
         profileImage.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(92)
             make.left.equalTo(view).inset(20)
-            make.top.equalTo(view).inset(136)
             make.width.equalTo(72)
             make.height.equalTo(72)
         }
         
         profileName.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(104)
             make.left.equalTo(view).inset(116)
-            make.top.equalTo(view).inset(148)
             make.width.equalTo(60)
             make.height.equalTo(24)
         }
         
         profileEmail.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(128)
             make.left.equalTo(view).inset(116)
-            make.top.equalTo(view).inset(172)
             make.width.equalTo(115)
             make.height.equalTo(24)
         }
@@ -246,9 +196,8 @@ final class ProfileViewController: UIViewController {
         }
         
         termsButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(view).inset(548)
-            make.width.equalTo(336)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.bottom.equalTo(view).inset(208)
             make.height.equalTo(56)
         }
         
@@ -267,9 +216,8 @@ final class ProfileViewController: UIViewController {
         }
         
         signoutButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(view).inset(632)
-            make.width.equalTo(336)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.bottom.equalTo(view).inset(124)
             make.height.equalTo(56)
         }
         
