75337e886a6f53f34449538608cbfb793fd0f5a9
michaelbolgar <mr.mbolgar@inbox.ru>
2024-03-27 17:28:29 +0100

    added search screen

--- NewsToday/NewsToday/Application/SceneDelegate.swift
+++ NewsToday/NewsToday/Application/SceneDelegate.swift
@@ -1,55 +1,31 @@
 import UIKit
 
 //class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-//
+//    
 //    var window: UIWindow?
 //    let factory: AppFactory = Factory()
-//
+//    
 //    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 //        guard let windowScene = (scene as? UIWindowScene) else { return }
 //        window = UIWindow(windowScene: windowScene)
-//
-//        let rootViewController: UIViewController
-//
-//        // Проверяем значение в UserDefaults
-//        if !UserDefaults.standard.bool(forKey: "hasLaunchedBefore") {
-//            // Если приложение открывается впервые, отображаем OnboardingViewController
-//            rootViewController = factory.makeOnboardingViewController()
-//        } else {
-//            // Если приложение уже открывалось, отображаем HomeViewController
-//            rootViewController = factory.makeHomeViewController()
-//        }
-//        window?.rootViewController = rootViewController
-//        window?.makeKeyAndVisible()
+//        let rootRouter = factory.makeRootRouter(window!)
+//        rootRouter.start()
 //    }
 //}
-
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
-    var window: UIWindow?
-    let factory: AppFactory = Factory()
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        let rootRouter = factory.makeRootRouter(window!)
-        rootRouter.start()
-    }
-}
 
 
 //загрузка ViewController
 
-//class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-//
-//    var window: UIWindow?
-//
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//
-//        let window = UIWindow(windowScene: windowScene)
-//        window.rootViewController = ViewController()
-//        window.makeKeyAndVisible()
-//        self.window = window
-//    }
-//}
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = SearchArticlesViewController(searchText: "apple")
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
--- NewsToday/NewsToday/Moduls/Details/View/DetailsViewController.swift
+++ NewsToday/NewsToday/Moduls/Details/View/DetailsViewController.swift
@@ -2,20 +2,20 @@ import UIKit
 import SnapKit
 
 protocol DetailsViewControllerProtocol: AnyObject {
-    func setupData(with testData: ([DetailsModel]))
+    func setupData(with data: ([Article]))
     func displayData()
 }
 
 final class DetailsViewController: UIViewController, DetailsViewControllerProtocol, UITextViewDelegate, UIScrollViewDelegate {
-    
+
     //MARK: - Presenter
     private let presenter = DetailsPresenter()
-    
-    
+
+
     //MARK: - Private properties
-    private var testData: [DetailsModel] = []
+    var data: [Article] = []
     weak private var detailsPresenterProtocol: DetailsPresenterProtocol?
-    
+
     //MARK: - UI Components
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -32,20 +32,20 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
        image.contentMode = .scaleAspectFill
         return image
     }()
-    
+
     private let shareButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: "share-icon"), for: .normal)
         return button
     }()
-    
+
     private let labelView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.purplePrimary
         view.layer.cornerRadius = 16
         return view
     }()
-    
+
     private let categoryLabel = UILabel.makeLabel(font: UIFont.InterSemiBold(ofSize: 12),
                                                   textColor: .white,
                                                   numberOfLines: 1)
@@ -60,7 +60,7 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
                                                     numberOfLines: 1)
     private let titleLabel = UILabel.makeLabel(font: UIFont.InterSemiBold(ofSize: 16),
                                                textColor: UIColor.blackPrimary,
-                                               numberOfLines: 1)
+                                               numberOfLines: 0)
     private let contentTextView: UITextView = {
         let textView = UITextView()
         textView.isEditable = false
@@ -69,41 +69,43 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
         textView.textColor = UIColor.greyDark
         return textView
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         setupConstraints()
         presenter.setDetailsViewControllerProtocol(detailsViewControllerProtocol: self)
         self.detailsPresenterProtocol = presenter
-        self.detailsPresenterProtocol?.getData()
+        self.detailsPresenterProtocol?.getData(with: data)
         configureController()
         setNavigationBar(title: "")
-     
+
     }
     // MARK: - Public Methods
-    func setupData(with testData: ([DetailsModel])) {
-        self.testData = testData
-        
+    func setupData(with data: ([Article])) {
+        self.data = data
+
     }
-    
+
     func displayData() {
-        categoryLabel.text = testData.first?.categoryLabel
-        headlineLabel.text = testData.first?.headlineLabel
-        authorLabel.text = testData.first?.authorLabel
-        titleLabel.text = testData.first?.titleLabel
-        contentTextView.text = testData.first?.contentTextView
-        
+        categoryLabel.text = data.first?.source.name
+        headlineLabel.text = data.first?.title
+        authorLabel.text = data.first?.author
+        titleLabel.text = data.first?.title
+        contentTextView.text = data.first?.content
+
     }
-    
+
     @objc func bookmarkButtonTapped() {
-        
+
     }
-    
+
     @objc func shareButtonTapped() {
-        
+        guard let textToShare = contentTextView.text else { return }
+        let activityViewController = UIActivityViewController(activityItems: [textToShare], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
     }
-    
+
     // MARK: Private methods
     private func setupViews() {
         [contentImage, shareButton, labelView, categoryLabel, headlineLabel, authorLabel, authorGreyLabel, titleLabel, contentTextView].forEach {contentView.addSubview($0) }
@@ -111,21 +113,22 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
         scrollView.addSubview(contentView)
         labelView.addSubview(categoryLabel)
     }
-    
+
     private func configureController() {
         let bookmarkButton = UIBarButtonItem(image: UIImage(named: "bookmark-icon"), style: .plain, target: self, action: #selector(bookmarkButtonTapped))
         bookmarkButton.tintColor = .white
         navigationItem.rightBarButtonItem = bookmarkButton
+        shareButton.addTarget(self, action: #selector(shareButtonTapped), for: .touchUpInside)
         view.backgroundColor = .systemBackground
         contentTextView.delegate = self
         scrollView.contentInsetAdjustmentBehavior = .never
     }
-    
+
     private func setupConstraints() {
         scrollView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
+
         contentView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
             make.width.equalTo(scrollView)
@@ -134,43 +137,44 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
         contentImage.snp.makeConstraints { make in
           make.top.equalToSuperview()
           make.leading.trailing.equalToSuperview()
-            
+
         }
         shareButton.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(120)
             make.trailing.equalToSuperview().offset(-16)
         }
-        
+
         labelView.snp.makeConstraints { make in
-            make.width.equalTo(75)
+            make.width.greaterThanOrEqualTo(32)
+            make.width.equalTo(categoryLabel.snp.width).offset(20)
             make.height.equalTo(32)
             make.top.equalToSuperview().offset(168)
             make.leading.equalToSuperview().offset(20)
         }
-        
+
         categoryLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
         }
-        
+
         headlineLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(216)
             make.leading.trailing.equalToSuperview().inset(20)
         }
-        
+
         authorLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(296)
             make.leading.equalToSuperview().offset(26)
         }
-        
+
         authorGreyLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(320)
             make.leading.equalToSuperview().offset(26)
         }
-        
+
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(contentImage.snp.bottom).offset(20)
-            make.leading.equalToSuperview().offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
         }
         contentTextView.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(5)
@@ -179,5 +183,5 @@ final class DetailsViewController: UIViewController, DetailsViewControllerProtoc
             make.bottom.equalToSuperview()
         }
     }
-    
+
 }
